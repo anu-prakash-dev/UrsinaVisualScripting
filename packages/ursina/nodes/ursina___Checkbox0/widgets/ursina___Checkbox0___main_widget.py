e3958d3e12b39d37d9ac0b47108565c74c698d03
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2021-05-09 00:35:33 +0200

    Created CheckBox and ColorPicker

---
+++ packages/ursina/nodes/ursina___Checkbox0/widgets/ursina___Checkbox0___main_widget.py
@@ -0,0 +1,40 @@
+from NIWENV import *
+
+
+# from PySide2.QtWidgets import ...
+# from PySide2.QtCore import ...
+# from PySide2.QtGui import ...
+
+
+from PySide2.QtWidgets import QCheckBox
+
+
+
+class Checkbox_NodeInstance_MainWidget(QCheckBox, MWB):
+    def __init__(self, params):
+        MWB.__init__(self, params)
+        QCheckBox.__init__(self)
+
+
+        
+        self.setStyleSheet('''
+        QCheckBox::indicator { 
+            width:50px; 
+            height: 50px; 
+            }
+        ''')
+        self.clicked.connect(M(self.parent_node_instance.stateChanged))
+
+
+    def get_data(self):
+        return {}
+
+
+    def set_data(self, data):
+        pass
+
+
+
+    def remove_event(self):
+        pass
+
