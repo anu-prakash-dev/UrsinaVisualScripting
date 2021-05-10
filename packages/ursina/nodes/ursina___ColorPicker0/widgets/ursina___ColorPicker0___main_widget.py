19c3cfa1c2d1a2d839fdd335c6e7c085d90b161e
Anatole Sot <47571181+ano0002@users.noreply.github.com>
2021-05-10 22:26:20 +0200

    Fixed ColorPicker Node not saving the color

--- packages/ursina/nodes/ursina___ColorPicker0/widgets/ursina___ColorPicker0___main_widget.py
+++ packages/ursina/nodes/ursina___ColorPicker0/widgets/ursina___ColorPicker0___main_widget.py
@@ -12,26 +12,26 @@ class ColorPicker_NodeInstance_MainWidget(QtWidgets.QPushButton, MWB):
         MWB.__init__(self, params)
         QtWidgets.QPushButton.__init__(self)
         self.clicked.connect(self.on_clicked)
-        self.setStyleSheet("border : none; background-color: (255,255,255,255)")
         self.color = (255,255,255,255)
         self.setFixedSize(110, 110)
 
     def on_clicked(self):
         color = QtWidgets.QColorDialog.getColor(options=QtWidgets.QColorDialog.ShowAlphaChannel)
         if color.isValid():
-            r,g,b,a = color.getRgb()
-            self.color = (r,g,b,a)
-            self.setStyleSheet("border : none; background-color: {}".format(color.name()))
+            self.color = color.getRgb()
+            self.setStyleSheet("border : none; background-color: rgb{};".format(self.color[:3]))
             self.parent_node_instance.update_color(self.color)
 
         
 
 
     def get_data(self):
-        return {}
+        return {"color" : self.color}
 
 
     def set_data(self, data):
+        self.color = tuple(data["color"])
+        self.setStyleSheet("border : none; background-color: rgb{};".format(self.color[:3]))
         pass
 
 
